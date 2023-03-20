17e5d132b9fe36e0cb5b62a040f1fb4d9a15a423
BCupen <bacupen@gmail.com>
2023-03-20 14:45:48 -0400

    api call implemented

--- main.bundle.js
+++ main.bundle.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
@@ -8,13 +9,23 @@
  */
 (self["webpackChunkweatherproject"] = self["webpackChunkweatherproject"] || []).push([["main"],{
 
+/***/ "./src/api.js":
+/*!********************!*\
+  !*** ./src/api.js ***!
+  \********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nclass weatherAPI {\n    _apiKey = '0acdadc54f11831d45863556db35e0d6';\n\n    getBasicCallURL (cityName){\n        return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${this._apiKey}`;\n    }\n\n    async getBasicData (url) {\n        const response = await fetch(url);\n        console.log(response);\n    }\n\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (weatherAPI);\n\n//# sourceURL=webpack://weatherproject/./src/api.js?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://weatherproject/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _api__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./api */ \"./src/api.js\");\n\n\nlet api = new _api__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\n\nconst url = api.getBasicCallURL('Port-Of-Spain');\n\napi.getBasicData(url);\n\n//# sourceURL=webpack://weatherproject/./src/index.js?");
 
 /***/ })
 
